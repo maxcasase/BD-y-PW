d8d25513e1c8ea45fb6d4acdd7f7349623413df2
maxcasase <maximiliano.casas@estudiantes.uv.cl>
2025-09-23 01:35:33 -0300

    Update albums.js

--- src/routes/albums.js
+++ src/routes/albums.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const { 
-  getAlbums, 
-  getAlbum, 
-  getAlbumReviews, 
-  createAlbum 
-} = require('../controllers/albumController');
+const { getAlbums, getAlbum, createAlbum } = require('../controllers/albumController');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/', getAlbums);
 router.get('/:id', getAlbum);
-router.get('/:id/reviews', getAlbumReviews);
-router.post('/', protect, createAlbum); // Solo usuarios autenticados
+router.post('/', protect, createAlbum);
 
 module.exports = router;
