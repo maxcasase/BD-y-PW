cd4ea325622939fa9e7cc9147c49b51f18daecdc
maxcasase <maximiliano.casas@estudiantes.uv.cl>
2025-09-23 01:36:39 -0300

    Update reviews.js

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,10 +1,5 @@
 const express = require('express');
-const { 
-  createReview, 
-  getReviews, 
-  updateReview, 
-  deleteReview 
-} = require('../controllers/reviewController');
+const { createReview, getReviews } = require('../controllers/reviewController');
 const { protect } = require('../middleware/auth');
 const { validateReview } = require('../middleware/validation');
 
@@ -12,7 +7,5 @@ const router = express.Router();
 
 router.get('/', getReviews);
 router.post('/', protect, validateReview, createReview);
-router.put('/:id', protect, validateReview, updateReview);
-router.delete('/:id', protect, deleteReview);
 
 module.exports = router;
