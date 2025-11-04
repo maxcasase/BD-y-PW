5e8d7ce140aadbc8e0751af7c9e3a17ac0f2ccd0
maxcasase <maximiliano.casas@estudiantes.uv.cl>
2025-11-04 04:43:05 -0300

    feat(reviews): add /me and DELETE /:id endpoints for complete review CRUD

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createReview, getReviews } = require('../controllers/reviewController');
+const { createReview, getReviews, getUserReviews, deleteReview } = require('../controllers/reviewController');
 const { protect } = require('../middleware/auth');
 const { validateReview } = require('../middleware/validation');
 
 const router = express.Router();
 
 router.get('/', getReviews);
+router.get('/me', protect, getUserReviews);
 router.post('/', protect, validateReview, createReview);
+router.delete('/:id', protect, deleteReview);
 
 module.exports = router;
